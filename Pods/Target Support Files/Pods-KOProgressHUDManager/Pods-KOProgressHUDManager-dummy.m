011317f2429eb53b81ecc64aa99f648e1b53ec88
Korune <rwxgood@126.com>
2018-08-28 22:37:58 +0800

    init project

---
+++ Pods/Target Support Files/Pods-KOProgressHUDManager/Pods-KOProgressHUDManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KOProgressHUDManager : NSObject
+@end
+@implementation PodsDummy_Pods_KOProgressHUDManager
+@end
---
+++ Pods/Target Support Files/Pods-KOProgressHUDManagerUITests/Pods-KOProgressHUDManagerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KOProgressHUDManagerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KOProgressHUDManagerUITests
+@end
