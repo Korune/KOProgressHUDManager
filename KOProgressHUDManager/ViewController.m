b74c7839cbdaae38a5ff1b92ba9a5f5da9ae18cf
Korune <rwxgood@126.com>
2018-08-28 23:42:46 +0800

    添加文件

--- KOProgressHUDManager/ViewController.m
+++ KOProgressHUDManager/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "KOProgressHUDManager.h"
 
 @interface ViewController ()
+
+- (IBAction)showSuccess:(id)sender;
+- (IBAction)showError:(id)sender;
+- (IBAction)showLoading:(id)sender;
+- (IBAction)hideHUD:(id)sender;
 
 @end
 
@@ -17,6 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"KOProgressHUDManager 演示";
 }
 
 
@@ -24,6 +31,25 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark -
+
+- (IBAction)showSuccess:(id)sender {
+    [KOProgressHUDManager showSuccess:@"小伙子，你成功了！"];
+}
+
+- (IBAction)showError:(id)sender {
+    [KOProgressHUDManager showError:@"小伙子，暂时失败了，不要灰心!" duration:2];
+}
+
+- (IBAction)showLoading:(id)sender {
+    [KOProgressHUDManager showLoading:@"小伙子，等等……" onView:self.view];
+}
+
+- (IBAction)hideHUD:(id)sender {
+    [KOProgressHUDManager hideHUDForView:self.view];
+}
+
 
 
 @end
